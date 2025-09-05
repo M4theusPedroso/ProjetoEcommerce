e59a1247a74cd25dff5231ebe5eb5b6d2b1dd174
Fernanda <fernandapimentel802@gmail.com>
2025-09-04 21:51:41 -0300

    Arquitetura de Sistemas

---
+++ API/api/src/main/java/br/com/ecommerce/api/controller/ClienteController.java
@@ -0,0 +1,30 @@
+package br.com.ecommerce.api.controller;
+
+import br.com.ecommerce.api.model.Cliente;
+import br.com.ecommerce.api.service.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+    // Controller -> Service
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService service) {
+        clienteService = service;
+    }
+
+    // listar todos
+    @GetMapping
+    public ResponseEntity<List<Cliente>> ListarClientes() {
+        //1 - pegar a lista de clientes
+        List<Cliente> clientes = clienteService.listarTodos();
+        return ResponseEntity.ok().body(clientes);
+
+    }
+}
---
+++ API/api/src/main/java/br/com/ecommerce/api/controller/ProdutoController.java
@@ -0,0 +1,26 @@
+package br.com.ecommerce.api.controller;
+
+
+import br.com.ecommerce.api.model.Produto;
+import br.com.ecommerce.api.service.ProdutoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produto")
+public class ProdutoController {
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService service) {
+        produtoService = service;
+    }
+    @GetMapping
+    public ResponseEntity<List<Produto>> listarProdutos() {
+        List<Produto> produtos = produtoService.ListarTodos();
+        return ResponseEntity.ok().body(produtos);
+    }
+}
