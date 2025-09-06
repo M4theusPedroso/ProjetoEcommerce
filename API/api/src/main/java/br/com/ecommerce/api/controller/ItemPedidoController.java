09deee6a6ddec7bb76bcc2ef4c72091e7a27162a
Fernanda <fernandapimentel802@gmail.com>
2025-09-05 21:10:19 -0300

    Atualizando Arquitetura de Projeto

---
+++ API/api/src/main/java/br/com/ecommerce/api/controller/ItemPedidoController.java
@@ -0,0 +1,26 @@
+package br.com.ecommerce.api.controller;
+
+import br.com.ecommerce.api.model.ItemPedido;
+import br.com.ecommerce.api.service.ItemPedidoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/itempedido")
+public class ItemPedidoController {
+    private final ItemPedidoService itemPedidoService;
+
+    public ItemPedidoController(ItemPedidoService service) {
+        itemPedidoService = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemPedido>> ListarItemPedido(){
+        List<ItemPedido> itemPedidos = itemPedidoService.listarTodos();
+        return ResponseEntity.ok().body(itemPedidos);
+    }
+}
---
+++ API/api/src/main/java/br/com/ecommerce/api/service/ItemPedidoService.java
@@ -0,0 +1,21 @@
+package br.com.ecommerce.api.service;
+
+import br.com.ecommerce.api.model.ItemPedido;
+import br.com.ecommerce.api.repository.ItemPedidoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemPedidoService {
+    private final ItemPedidoRepository itemPedidoRepository;
+
+    public ItemPedidoService(ItemPedidoRepository item) {
+        itemPedidoRepository = item;
+    }
+
+    public List<ItemPedido> listarTodos() {
+        return itemPedidoRepository.findAll();
+    }
+
+}
